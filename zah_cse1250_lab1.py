a703cf898b70428e3a4330b66d7285ec321a2414
Zollicoff <zollicoff@gmail.com>
2023-09-07 20:46:39 -0700

    done

--- zah_cse1250_lab1.py
+++ zah_cse1250_lab1.py
@@ -6,14 +6,15 @@
 # This program lets a user type and display their name and is greeted
 
 # user is prompted to input name
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
 
 # display user's name and welcome message
 print()
-print("Hello, " + first_name + " " + last_name + "!")
+print("Hello, " + firstName + " " + lastName + "!")
 print("Welcome to Python!")
 
 # exit message
 print()
 print("Have a nice day!")
+print()
