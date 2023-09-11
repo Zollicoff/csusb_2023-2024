d6934e78451f897ee091f72641692c72b190ff41
Zollicoff <zollicoff@gmail.com>
2023-09-11 10:22:06 -0700

    zzzzz

---
+++ test.r
@@ -0,0 +1,10 @@
+# initialize a variable to hold the sum
+sum <- 0
+
+# loop through the first 10 natural numbers and add them to the sum
+for (i in 1:10) {
+    sum <- sum + i
+}
+
+# print the sum
+print(sum)
