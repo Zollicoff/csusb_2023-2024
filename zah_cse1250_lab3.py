98e3f8aebb35fb7268572efe2ead058378025ef0
Zollicoff <zollicoff@gmail.com>
2023-09-11 21:31:16 -0700

    tickle

---
+++ zah_cse1250_lab3.py
@@ -0,0 +1,25 @@
+# Zachary Hampton
+# September 11, 2023
+# Lab Section 83005
+# Lab 3
+
+# This program lets a user enter any integer base and integer exponent and displays the result
+print("This program will let you raise any integer to any integer power.")
+print("Remmber to use only Whole numbers.")
+print()
+
+# User input for the base and exponent
+base = int(input("Please enter the base Number? "))
+exponent = int(input("What power of " + str(base) + "? "))
+print()
+
+# The calculation process
+result = base ** exponent
+
+# Output displays the result of the calculation
+print(base, "to the power of", exponent, "is", result)
+print()
+
+# Exit message
+print("Thanks you for using the Power Calculator!")
+print("Have a nice day!")
