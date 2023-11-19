cd2314c2ebf004fad932e09eda0e5a87bb4e4d1d
Zachary A. Hampton <95104559+Zollicoff@users.noreply.github.com>
2023-11-19 03:52:29 +0000

    -

--- zah_cse1250_lab12-cs.py
+++ zah_cse1250_lab12-cs.py
@@ -5,12 +5,14 @@
 
 # This program will convert degrees Celsius to degrees Fahrenheit.
 
+# define a function to display a user greeting
 def user_greeting():
     # Welcome message and user instructions for program
     print("This program will convert degrees Celsius to degrees Fahrenheit.")
     print("Please enter the temperature in Celsius; you may use decimals.")
     print()
 
+# define a function to ask the user if they want to run the program again
 def ask_again():
     while True:
         response = input("Do you want to run the program again? (yes/no): ")
@@ -28,7 +30,7 @@ run_program = True
 # Main program loop
 while run_program:
     try:
-        # Get user input for degrees Celsius
+        # Get user input for degrees Celsius and convert to float, while checking for errors
         celsius = float(input("Enter a temperature in degrees Celsius: "))
     except ValueError:
         print("Invalid input. Please enter a number.")
