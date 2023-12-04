96d24ce4580e405d27319a63ba6bc8bda92cc939
Zachary A. Hampton <95104559+Zollicoff@users.noreply.github.com>
2023-12-04 12:06:54 +0000

    -

--- zah_cse1250_lab14_final.py
+++ zah_cse1250_lab14_final.py
@@ -14,19 +14,40 @@ def get_product_info():
     product_info = []
     item_quantity = 1
     while item_quantity != 0:
-        item_quantity = int(input("\nEnter item quantity (enter 0 if done): "))
+        while True:
+            try:
+                item_quantity = int(input("\nEnter item quantity (enter 0 if done): "))
+                break
+            except ValueError:
+                print("Invalid input. Please enter a number.")
         if item_quantity != 0:
-            item_weight = float(input("Enter item weight: "))
-            item_cost = float(input("Enter cost of item: "))
+            while True:
+                try:
+                    item_weight = float(input("Enter item weight: "))
+                    break
+                except ValueError:
+                    print("Invalid input. Please enter a number.")
+            while True:
+                try:
+                    item_cost = float(input("Enter cost of item: "))
+                    break
+                except ValueError:
+                    print("Invalid input. Please enter a number.")
             product_info.append([item_quantity, item_weight, item_cost])
     return product_info
 
 # Define get state function
 def get_state():
-    states = ['CA']
-    state = input("Enter your state abbreviation: ")
+    states = [
+        'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 
+        'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN',
+        'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND',
+        'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT',
+        'VA', 'WA', 'WV', 'WI', 'WY'
+    ]
+    state = input("Please enter the 2 letter abbreviation of the state you are shipping to: ").upper()
     while state not in states:
-        state = input("Invalid state. Please enter a valid state abbreviation: ")
+        state = input("\nERROR- Please enter the 2 letter abbreviation of the state you are shipping to: ").upper()
     return state
 
 # Define calculate function
@@ -49,9 +70,11 @@ def calculate(product_info):
 
 # Define run again function
 def run_again():
-    run = input("Would you like to run the program again? (y/n): ")
+    run = input("\nWould you like to fill another order? (y/n): ")
     while run not in ['y', 'n']:
         run = input("Invalid input. Please enter 'y' or 'n': ")
+    if run == 'n':
+        print("\nThanks for using this program. Have a nice day!")
     return run == 'y'
 
 # Define main function
@@ -64,6 +87,7 @@ def main():
         state = get_state()
         tax = sub_total * .08 if state == 'CA' else 0
         total_due = sub_total + shipping_handling + tax
+        print()
         print(format('Subtotal:', '<25'), format(sub_total, '>10,.2f'))
         print(format('Tax:', '<25'), format(tax, '>10,.2f'))
         print(format('Shipping and Handling:', '<25'), format(shipping_handling, '>10,.2f'))
